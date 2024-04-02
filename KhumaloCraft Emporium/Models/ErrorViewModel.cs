00fede3ff4a99bd32739bf9793eacc3d297ba10d
1 Ethan <1 Ethan@DESKTOP-KF5T3VA>
2024-04-02 17:32:23 +0100

    Add project files.

---
+++ KhumaloCraft Emporium/Models/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+namespace KhumaloCraft_Emporium.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+    public class myWork
+    {
+
+    }
+}
